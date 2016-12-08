b88e3a4ba52febba58a8ce3a64a8adb9d50d8dc9
Svetlana Linuxenko <linuxenko@yahoo.com>
2016-12-08 21:33:52 +0200

    coveralls

--- tests/bootstrap.test.js
+++ tests/bootstrap.test.js
@@ -1,9 +1,10 @@
 var expect = require('chai').expect;
+var idx = require('../lib/');
 
 describe('Test cakejs bootstrap', function() {
 
   it('should run tests', function() {
-    expect(1 + 1).to.be.equal(2);
+    expect(idx(1, 1)).to.be.equal(2);
   });
 
 });
