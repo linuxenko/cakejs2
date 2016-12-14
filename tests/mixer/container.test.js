439902aabf94a3c7dc50444ddf25aa22e640541c
Svetlana Linuxenko <linuxenko@yahoo.com>
2016-12-15 00:19:49 +0200

    Sugar functions for Cream

--- tests/mixer/container.test.js
+++ tests/mixer/container.test.js
@@ -4,6 +4,12 @@ var Bakery = require('../../');
 describe('Container for ingredients', function() {
   afterEach(function() {
     try {
+      var creams = Object.keys(Bakery._container.creams);
+
+      for (var i = 0; i < creams.length; i++) {
+        Bakery.unregister('creams.' + creams[i]);
+      }
+
       Bakery.unregister('creams');
     } catch(e) { /* not found */ }
   });
@@ -51,8 +57,8 @@ describe('Container for ingredients', function() {
   });
 
   it('should hanlde unregistering invalid objects', function() {
-    expect(function() { Bakery.unregister('crms.ctrst'); }).to.throw();
-    expect(function() { Bakery.unregister('crms'); }).to.throw();
+    expect(Bakery.unregister('crms.ctrst')).to.be.an('undefined');
+    expect(Bakery.unregister('crms')).to.be.an('undefined');
   });
 
   it('should inject objects', function() {
@@ -66,5 +72,69 @@ describe('Container for ingredients', function() {
 
     expect(c2.c1).to.be.equal(c1);
     expect(c2.hello).to.be.equal('world');
+  });
+
+  it('should register object after', function() {
+    var c1 = Bakery.Cream.extend({ hello : 'world' });
+    var c2 = Bakery.Cream.extend({ world : 'hello' });
+
+    Bakery.register('creams.cream_1', c2, 'creams.cream_0');
+    expect(Bakery._container.creams).to.be.an('undefined');
+    Bakery.register('creams.cream_0', c1);
+    expect(Bakery._container.creams.cream_1).to.be.equal(c2);
+  });
+
+  it('should register after with any order', function() {
+    var c1 = Bakery.Cream.extend({ hello : 'world' });
+    var c2 = Bakery.Cream.extend({ world : 'hello' });
+
+    Bakery.register('creams.cream_0', c1);
+    Bakery.register('creams.cream_1', c2, 'creams.cream_0');
+
+    expect(Bakery.inject('creams.cream_1')).to.be.equal(c2);
+  });
+
+  it('should inject even before object registered', function() {
+    var c1 = Bakery.Cream.extend({ hello : Bakery.inject('creams.injectable2'),
+      test: 'test',
+      testFn : function() { }
+    });
+    var c2 = Bakery.Cream.extend({ world : 'texthello' });
+
+    Bakery.register('creams.cream_0', c1);
+    Bakery.register('creams.injectable2', c2);
+
+    expect(c1.get('hello.world')).to.be.equal('texthello');
+    expect(c1.get('testFn')).to.be.a('function');
+    expect(c1.get('testFn.test')).to.be.an('undefined');
+  });
+
+  it('should register object\'s namespace', function() {
+    var c = Bakery.Cream.extend({});
+
+    Bakery.register('creams.cream_0', c);
+    expect(c._namespace).to.be.equal('creams.cream_0');
+  });
+
+  it('should register selfnamespaced creams', function() {
+    var c = Bakery.Cream.extend({
+      _namespace : 'creams.selfreg'
+    });
+
+    expect(Bakery._container.creams.selfreg).to.be.equal(c);
+    expect(c._namespace).to.be.equal(c.get('_namespace'));
+  });
+
+  it('should register/unregister observers', function() {
+    var c = Bakery.Cream.extend({
+      _namespace : 'creams.observable_cream',
+      hello : 'world',
+      test : function() { return this.get('hello'); }.observes('hello')
+    });
+
+    expect(c).to.be.instanceof(Bakery.Cream);
+    expect(c.get('test')).to.be.equal('world');
+
+    Bakery.unregister('creams.observable_creams');
   });
 });
--- tests/recipes/cream.test.js
+++ tests/recipes/cream.test.js
@@ -3,6 +3,18 @@ var Bakery = require('../../');
 var Cream = Bakery.Cream;
 
 describe('Cream for the cake', function() {
+  afterEach(function() {
+    try {
+      var creams = Object.keys(Bakery._container.creams);
+
+      for (var i = 0; i < creams.length; i++) {
+        Bakery.unregister('creams.' + creams[i]);
+      }
+
+      Bakery.unregister('creams');
+    } catch(e) { /* not found */ }
+  });
+
   it('should create some cream', function() {
     var c = Cream.extend({ str : 'str', init : true });
 
@@ -20,5 +32,68 @@ describe('Cream for the cake', function() {
     expect(Cream.str).to.be.an('undefined');
     expect(d.str).to.be.an('undefined');
     expect(d.opt).to.be.false;
+  });
+
+  it('should handle different getters', function() {
+    var c = Cream.extend({
+      hello : 'world',
+      nested : {
+        test : 'text'
+      }
+    });
+
+    expect(c.get('hello')).to.be.equal('world');
+    expect(c.get('nested.test')).to.be.equal('text');
+  });
+
+  it('should handle embedded function bindings', function() {
+    var c = Cream.extend({
+      hname : 'hello',
+      test : function() { return this.get('hname'); }
+    });
+
+    expect(c.get('test')()).to.be.equal('hello');
+    expect(c.test()).to.be.equal('hello');
+  });
+
+  it('should create property functions', function() {
+    var c = Cream.extend({
+      hello : 'hello',
+      test : function() { return this.get('hello') + 'world'; }.property()
+    });
+
+    expect(c.get('test')).to.be.equal('helloworld');
+  });
+
+  it('should create observer function', function() {
+    var up = null;
+    var c = Cream.extend({
+      _namespace : 'creams.observable_cream',
+      hello : 'hello',
+      test : function() { up = this.get('hello'); }.observes('hello')
+    });
+
+    expect(c.get('hello')).to.be.equal('hello');
+    expect(up).to.be.null;
+
+    c.set('hello', 'world');
+    expect(c.get('hello')).to.be.equal('world');
+    expect(up).to.be.equal('world');
+  });
+
+  it('should cream objects communicate with observers', function() {
+    var b = Cream.extend({
+      _namespace : 'creams.b',
+      test : 'hello'
+    });
+
+    var c = Cream.extend({
+      _namespace : 'creams.c',
+      btest : Bakery.inject('creams.b'),
+      watch : function() { console.log('aaa'); }.observes('btest')
+    });
+
+    b.set('test', 'world');
+
   });
 });
